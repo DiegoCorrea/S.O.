e88aa8d8437970082a75f6290975b99be538f0e4
Diego Correa <diego.darkcastle@gmail.com>
2014-11-12 23:07:50 -0300

    lendo todas as variaveis de entrada

--- sched.c
+++ sched.c
@@ -5,18 +5,23 @@
 
 int main(int argc, char *argv[])
 {
+	printf("O nome do arquivo de ENTRADA é: %s\n", argv[2]);
+	printf("O nome do arquivo de SAIDA é: %s\n", argv[3]);
 
 	if(strcmp("FCFS",argv[1])==0)
+	{
 		printf ("Você escolheu o FCFS\n");
-		
+	}	
 		else if (strcmp("RR",argv[1])==0)
+		{
 			printf ("Você escolheu o RR\n");
-
+		}
 			else if (strcmp("SJF",argv[1])==0)
+		{
 				printf ("Você escolheu o SJF\n");
-			
+		}
 			else
-				printf ("Você não escolheu nenhuma opção válida! Finalizando processo");
+				printf ("Você não escolheu nenhuma opção válida! Finalizando processo\n");
 
 	return EXIT_SUCCESS;
 }
