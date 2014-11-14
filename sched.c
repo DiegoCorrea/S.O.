931282939b7d4a999175ababde85bba5a29ddb64
Diego Correa <diego.darkcastle@gmail.com>
2014-11-13 23:01:01 -0300

    dados inseridos na tabela

--- sched.c
+++ sched.c
@@ -1,24 +1,25 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#define MAX 50
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# define MAX 50
 
-typedef struct processos
+typedef struct proc
 {
-   int  id;			//ID do processo
-   int  chegada;	//Instante de chegada
-   int  tempo;		//Tempo de processamento
-   int  oiI;		//Instante que inicia o IO
-   int  oiT;		//Instante que termina o IO
-   struct   processos *ant;
-   struct   processos *prox;
-};
+    int  id;			//ID do processo
+    int  chegada;		//Instante de chegada
+    int  tempo;			//Tempo de processamento
+    int  ioI;			//Instante que inicia o IO
+    int  ioT;			//Instante que termina o IO
+    struct   proc *ant;
+    struct   proc *prox;
+}proc;
 
+void Inserir_tabela(proc *processos, char linha[30]);
 
 int main(int argc, char *argv[])
 {
+	proc *processos = NULL;
 	char linha[20];
-	int l,c, j, a, b;
 
 	printf("O nome do arquivo de ENTRADA é: %s\n", argv[2]);
 	printf("O nome do arquivo de SAIDA é: %s\n", argv[3]);
@@ -42,114 +43,95 @@ int main(int argc, char *argv[])
 	//retirando itens do arquivo
     FILE *fl_entrada = fopen(argv[2], "r" );
 
-    if ( fl_entrada == 0 || entrada == NULL) {
+    if ( fl_entrada == 0 || fl_entrada == NULL) {
 	  fprintf(stderr, "Arquivo %s não encontrado\n", argv[1]);
 	  exit(1);
 	}
     else 
     {
-    	l=0;
 		while (fscanf(fl_entrada, "%s", linha) != EOF) 
 		{
-			j = 0;
-			c = 0;
 			printf("Linha do arquivo: %s\n", linha);
-			processo->id = linha[0];
-			processo->chegada = linha[2];
-			processo->tempo = linha[4];
-
-			if(linha[6] == ";")
-			{
-				processo->ioI = 0;
-				processo->ioT = 0;
-			}
-			else
-			{
-				processo->ioI = linha[6];
-				processo->ioT = linha[8];
-			}
-
-			while(linha[j] != '\n')
-			{
-				if (linha[j] != ';')
-				{
-					tabela[l][c] = atoi(&linha[j]);
-					c++;
-				}
-				if (j > 0 && linha[j] == ';' && linha[j-1] == ';')
-				{
-					tabela[l][c] = 0;
-					c++;
-				}
-				j++;
-			}			
-			l++;
+			Inserir_tabela(processos,linha);
 		}
-			a =0;
-			b =0;
-			while(a < l)
-			{
-				b = 0;
-				while(b < 5)
-				{
-				printf("%d \n", tabela[a][4]);				
-				b++;
-				}
-				a++;
-			}
         fclose( fl_entrada );
     }
 	return EXIT_SUCCESS;
 }
 
-/*
-void criando_tabela(char *argv)
+void Inserir_tabela(proc *processos, char linha[30])
 {
-     FILE *entrada = fopen(argv[2], "r" );
+	proc *processo;
+	int i = 0, j= 0;
+	char buffer[8];
 
-    if ( entrada == 0 || entrada == NULL) {
-	  fprintf(stderr, "Arquivo %s não encontrado\n", argv[1]);
-	  exit(1);
+	processo = (proc*)malloc(sizeof(proc));
+	processo->ant = NULL;
+	processo->prox = NULL;
+	
+	while(linha[i] != ';')
+	{
+		buffer[j] = linha[i];
+		i++;
+		j++;
 	}
-    else 
-    {
-    	l=0;
-		while (fscanf(entrada, "%s", linha) != EOF) 
+	buffer[j] = '\0';
+	processo->id = atoi(buffer);
+	printf("%d\n", processo->id);
+	j = 0;
+	i++;
+	while(linha[i] != ';')
+	{
+		buffer[j] = linha[i];
+		i++;
+		j++;
+	}
+	buffer[j] = '\0';
+	processo->chegada = atoi(buffer);
+	printf("%d\n",processo->chegada);
+	j = 0;
+	i++;
+	while(linha[i] != ';')
+	{
+		buffer[j] = linha[i];
+		i++;
+		j++;
+	}
+	buffer[j] = '\0';
+	processo->tempo = atoi(buffer);
+	printf("%d\n",processo->tempo);
+	j = 0;
+	i++;
+	
+	if( linha[i] != ';' )
+	{	
+		while(linha[i] != ',')
 		{
-			j = 0;
-			c = 0;
-			printf("Linha do arquivo: %s\n", linha);
-
-			while(linha[j] != '\n')
-			{
-				if (linha[j] != ';')
-				{
-					tabela[l][c] = atoi(&linha[j]);
-					c++;
-				}
-				if (j > 0 && linha[j] == ';' && linha[j-1] == ';')
-				{
-					tabela[l][c] = 0;
-					c++;
-				}
-				j++;
-			}			
-			l++;
+			buffer[j] = linha[i];
+			i++;
+			j++;
 		}
-			a =0;
-			b =0;
-			while(a < l)
-			{
-				b = 0;
-				while(b < 5)
-				{
-				printf("%d \n", tabela[a][4]);				
-				b++;
-				}
-				a++;
-			}
-        fclose( entrada );
-    }
-    
+		buffer[j] = '\0';
+		processo->ioI = atoi(buffer);
+		printf("%d\n",processo->ioI);
+		j = 0;
+		i++;
+		while(linha[i] != ';')
+		{
+			buffer[j] = linha[i];
+			i++;
+			j++;
+		}
+		buffer[j] = '\0';
+		processo->ioT = atoi(buffer);
+		printf("%d\n",processo->ioT);
+	}
+	else
+	{
+		processo->ioI = 0;
+		processo->ioT = 0;
+		printf("%d\n",processo->ioT);
+		printf("%d\n",processo->ioI);
+
+	}
 }
-*/
